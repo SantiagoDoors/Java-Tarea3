989ee0c1016be4d3f6135856b8cd087825c5420c
Santi <antratocx@gmail.com>
2023-03-12 18:26:18 +0100

    completo

--- C3_Ejercicio_4.java
+++ C3_Ejercicio_4.java
@@ -3,14 +3,23 @@ import javax.swing.JOptionPane;
 
 public class C3_Ejercicio_4 {
     public static void main(String[] args) {
+
+    // Declarando lo necesario para trabajar con el cifrado por desplazamiento
+
         String Cifrado = "";
         String Frase = JOptionPane.showInputDialog("Introduzca su frase a ser cifrada (sin acentos, ni ñ)");
         String desplazamiento = JOptionPane.showInputDialog("introduzca la cantidad de desplazamiento del cifrado");
+    
+    /*  Recorriendo la frase para traducirla a ASCII y para luego
+        desplazar de lugar en su forma entera a la letra y retraducirla codificada  */
 
         for (int i = 0; i < Frase.length(); i++){
             int Letras = (int)(Frase.charAt(i));
             Cifrado = Cifrado+String.valueOf((char)(Letras+Integer.valueOf(desplazamiento)));
         }
+
+    // Mostrando los resultados.
+
         System.out.println("su Frase es: "+Frase);
         System.out.println("eligió desplazar las letras: "+Integer.valueOf(desplazamiento)+" veces");
         System.out.println("Su Frase Cifrada es: "+Cifrado);
